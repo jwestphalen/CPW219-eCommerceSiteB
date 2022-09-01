dccdd6597f14c1d67a235dd263a9a0c57b083c97
Joshua Westphalen <joshua.westphalen@yahoo.com>
2022-08-31 21:04:37 -0700

    Added DBContext line for DefaultConnection

--- CPW219-eCommerceSiteB/Program.cs
+++ CPW219-eCommerceSiteB/Program.cs
@@ -1,7 +1,12 @@
+using CPW219_eCommerceSite.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<VideoGameContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
