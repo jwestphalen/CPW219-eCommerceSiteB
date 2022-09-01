7bca4555899dae0eed3acebcdfc882c9bba0f4b2
Joshua Westphalen <joshua.westphalen@yahoo.com>
2022-09-01 14:32:08 -0700

    Added Register IActionResult

---
+++ CPW219-eCommerceSiteB/Controllers/MembersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CPW219_eCommerceSiteB.Controllers
+{
+    public class MembersController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
