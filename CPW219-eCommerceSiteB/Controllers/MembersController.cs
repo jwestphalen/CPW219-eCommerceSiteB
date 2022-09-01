73f0933a2e611c14217c4f7907d51f21b9567833
Joshua Westphalen <joshua.westphalen@yahoo.com>
2022-09-01 15:20:03 -0700

    Basic login page functional

--- CPW219-eCommerceSiteB/Controllers/MembersController.cs
+++ CPW219-eCommerceSiteB/Controllers/MembersController.cs
@@ -39,5 +39,34 @@ namespace CPW219_eCommerceSiteB.Controllers
             
             return View(regModel);
         }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel loginModel)
+        {
+            if (ModelState.IsValid) 
+            { 
+                // Check DB for credentials
+                Member? m = (from member in _context.Members
+                           where member.Email == loginModel.Email &&
+                           member.Password == loginModel.Password 
+                           select member).SingleOrDefault();
+                
+                // If Exists, send to homepage
+                if (m != null)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Credentials not found!");
+            }
+            // Return page if no record found, or ModelState is invalid
+            return View(loginModel);
+        }
     }
 }
