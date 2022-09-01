056bd16cba9955b8f43b9c129ba23ad349b00587
Joshua Westphalen <joshua.westphalen@yahoo.com>
2022-09-01 13:31:53 -0700

    Added success message

--- CPW219-eCommerceSiteB/Controllers/GamesController.cs
+++ CPW219-eCommerceSiteB/Controllers/GamesController.cs
@@ -65,6 +65,7 @@ namespace CPW219_eCommerceSiteB.Controllers
                 _context.Games.Update(gameModel);
                 await _context.SaveChangesAsync();
 
+                TempData["Message"] = $"{gameModel.Title} was updated successfully";
                 return RedirectToAction("Index");
             }
 
