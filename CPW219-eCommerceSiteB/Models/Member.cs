ef591103dd0b627cdae8418ec845cecd551d5ee2
Joshua Westphalen <joshua.westphalen@yahoo.com>
2022-09-01 14:53:35 -0700

    Made member phone and username optional

--- CPW219-eCommerceSiteB/Models/Member.cs
+++ CPW219-eCommerceSiteB/Models/Member.cs
@@ -5,15 +5,15 @@ namespace CPW219_eCommerceSiteB.Models
     public class Member
     {
         [Key]
-        public int MemberId { get; set; }   
+        public int MemberId { get; set; }
 
-        public string Email { get; set; }
+        public string Email { get; set; } = null!;
 
-        public string Password { get; set; }   
+        public string Password { get; set; } = null!;
 
-        public string Phone { get; set; }
+        public string? Phone { get; set; }
 
-        public string Username { get; set; }
+        public string? Username { get; set; }
     }
 
     public class RegisterViewModel
