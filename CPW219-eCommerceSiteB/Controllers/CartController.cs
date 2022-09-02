6f408f597d9fb13ea1de0fec8df669135df9e021
Joshua Westphalen <joshua.westphalen@yahoo.com>
2022-09-01 18:35:35 -0700

    Added Functionality for multiple items in cart

--- CPW219-eCommerceSiteB/Controllers/CartController.cs
+++ CPW219-eCommerceSiteB/Controllers/CartController.cs
@@ -1,12 +1,14 @@
 ﻿using CPW219_eCommerceSite.Data;
 using CPW219_eCommerceSite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CPW219_eCommerceSiteB.Controllers
 {
     public class CartController : Controller
     {
         private readonly VideoGameContext _context;
+        private const string Cart = "ShoppingCart";
 
         public CartController(VideoGameContext context)
         {
@@ -24,9 +26,44 @@ namespace CPW219_eCommerceSiteB.Controllers
                 return RedirectToAction("Index", "Games");
             }
 
+            CartGameViewModel cartGame = new()
+            {
+                GameId = gameToAdd.GameId,
+                Title = gameToAdd.Title,
+                Price = gameToAdd.Price
+                
+            };
+
+            List<CartGameViewModel> cartGames = GetExistingCartData();
+            cartGames.Add(cartGame);
+
+            string cookieData = JsonConvert.SerializeObject(cartGames);
+
+            HttpContext.Response.Cookies.Append("ShoppingCart", cookieData, new CookieOptions()
+            {
+                Expires = DateTimeOffset.Now.AddYears(1)
+            });
             // ToDo: Add item to a shopping cart cookie
             TempData["Message"] = "Item added to cart";
             return RedirectToAction("Index", "Games");
+        }
+
+        /// <summary>
+        /// Return the current list of video games in the user's shopping cart 
+        /// cookie. If there is no cookie, an empty list will be returned.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        private List<CartGameViewModel> GetExistingCartData()
+        {
+            string? cookie = HttpContext.Request.Cookies[Cart];
+
+            if (string.IsNullOrWhiteSpace(cookie))
+            {
+                return new List<CartGameViewModel>();
+            }
+
+            return JsonConvert.DeserializeObject<List<CartGameViewModel>>(cookie);
         }
     }
 }
--- CPW219-eCommerceSiteB/Models/Game.cs
+++ CPW219-eCommerceSiteB/Models/Game.cs
@@ -25,4 +25,17 @@ namespace CPW219_eCommerceSite.Models
 
         // Todo: Add Rating
     }
+
+    /// <summary>
+    /// A single video game that has been added to the users
+    /// shopping cart cookie
+    /// </summary>
+    public class CartGameViewModel
+    {
+        public int GameId { get; set; }
+
+        public string Title { get; set; }
+
+        public double Price { get; set; }
+    }
 }
